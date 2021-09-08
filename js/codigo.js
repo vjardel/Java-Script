f3aec38a1b0ad5e6a6239fee2363cc0ff6a9fa74
Valentin Jardel <valentin.jaar@gmail.com>
2021-09-08 17:26:23 -0300

    Variables, alerts y console

---
+++ js/codigo.js
@@ -0,0 +1,21 @@
+console.log("Hola a todos");
+alert("Bienvenido a mi pagina");
+let nombreIngresado = prompt("Ingrese su nombre");
+let ingresado = nombreIngresado + " " + "¿Es su nombre?";
+alert(ingresado);
+
+let entrada = prompt("Ingrese su fecha de nacimiento");
+let salida  = entrada + " " + "¿Es su fecha de nacimiento?";
+alert(salida);
+
+
+var   name = "Valentin";
+let   lastName = "Jardel";
+const BLANCO = " ";
+let nombreCompleto = name + BLANCO + lastName;
+let nombre = name;
+let apellido = lastName;
+console.log(nombreCompleto)
+console.log(nombre)
+console.log(apellido)
+
