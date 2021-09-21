0e8ca2fd5f731b1e23621fb45e717e542295ad8a
Valentin Jardel <valentin.jaar@gmail.com>
2021-09-20 22:18:34 -0300

    simulador interactivo

--- ej.js
+++ ej.js
@@ -1,22 +1,22 @@
-//let cantidadPersonas = parseInt(prompt("¿Cuantas Personas van a ir a la fiesta?"));
+/*let cantidadPersonas = parseInt(prompt("¿Cuantas Personas van a ir a la fiesta?"));
 
-//if (cantidadPersonas <=5) {alert (`Me parece que ${cantidadPersonas} personas son muy pocas para hacer una fiesta`);}
-//else if (cantidadPersonas <=20) {alert (`Me parece que con ${cantidadPersonas} personas podria ser una mini fiesta`);}
-//else if (cantidadPersonas <=60){alert (`Me parece que con ${cantidadPersonas} personas podria se una buena fiesta`);}
-//else if (cantidadPersonas <=100){alert (`Me parece que con ${cantidadPersonas} personas va a ser una gran fiesta`);}
-//else if(cantidadPersonas <=500) {alert  (`Espero que tengas una gran casa porque con ${cantidadPersonas} personas te va a explotar`);}
-//for (let i = 1; i <= 10 ; i++) {alert (i);}
-//let ingresarNumero = parseInt(prompt("Ingresar Numero"));
-//for (let i = 1; i <= 10; i++) {
-//    let resultado = ingresarNumero * i ;
-//    alert(ingresarNumero +" X "+ i +" = "+ resultado);}
+if (cantidadPersonas <=5) {alert (`Me parece que ${cantidadPersonas} personas son muy pocas para hacer una fiesta`);}
+else if (cantidadPersonas <=20) {alert (`Me parece que con ${cantidadPersonas} personas podria ser una mini fiesta`);}
+else if (cantidadPersonas <=60){alert (`Me parece que con ${cantidadPersonas} personas podria se una buena fiesta`);}
+else if (cantidadPersonas <=100){alert (`Me parece que con ${cantidadPersonas} personas va a ser una gran fiesta`);}
+else if(cantidadPersonas <=500) {alert  (`Espero que tengas una gran casa porque con ${cantidadPersonas} personas te va a explotar`);}
+for (let i = 1; i <= 10 ; i++) {alert (i);}
+let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero * i ;
+    alert(ingresarNumero +" X "+ i +" = "+ resultado);}
 
-//for (let i = 1; i <= 3; i++) {
-//    let ingresarNombre = prompt("Ingresar nombre");
-//    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre);}
+for (let i = 1; i <= 3; i++) {
+    let ingresarNombre = prompt("Ingresar nombre");
+    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre);}
 
 
-/*let nombreIngresado = prompt("Nombre");
+let nombreIngresado = prompt("Nombre");
 const MAXIMO_INTENTOS = 3 ;
 while (nombreIngresado !== ""){
     if (nombreIngresado === ""){
@@ -26,7 +26,7 @@ while (nombreIngresado !== ""){
         alert ("Hola Ema");
     }else{
         alert("Hola");
-        break;}}*/
+        break;}}
 
 //let numeroNotas = parseInt(prompt("Numero de notas a ingresar"));
 //let sumaTotal = 0;
@@ -38,7 +38,7 @@ while (nombreIngresado !== ""){
 //promedio = sumaTotal / numeroNotas;
 //alert(promedio);
 
-/*const CANTIDAD_NUMEROS = 1;
+const CANTIDAD_NUMEROS = 1;
 for(let i = 1; i <= CANTIDAD_NUMEROS; i++) {
     let numero = parseInt(prompt("Ingrese el numero"));
     switch (numero){
@@ -65,9 +65,9 @@ for(let i = 1; i <= CANTIDAD_NUMEROS; i++) {
             alert("No has ingresado un valor valido");
             break;
     }
-}*/
+}
 
-/*const CANTIDAD_NUMEROS = 1;
+const CANTIDAD_NUMEROS = 1;
 for(let i = 1; i <= CANTIDAD_NUMEROS; i++) {
     let numero = parseInt(prompt("Ingrese el numero"));
     switch (true){
@@ -90,4 +90,109 @@ for(let i = 1; i <= CANTIDAD_NUMEROS; i++) {
             alert("No has ingresado un valor valido");
             break;
     }
-}*/
+}
+
+let entrada = prompt("Ingresar un nombre");
+while(entrada != "ESC"){
+   switch (entrada) {
+       case "Brian":
+            alert("Bienvenido Brian");
+            break;
+        case "Valentin":
+            alert("Hola Valentín, bienvenido a tu página");
+            break;
+       default:
+           alert("No eres bienvenido a esta página")
+           break;
+   }
+   entrada = prompt("Ingresar un nombre");
+}
+
+
+for (let i = 1; i <= 2; i++) {
+    let ingresarNombre = prompt("Ingresar nombre");
+    let ingresarApellido = prompt("Ingresar apellido")
+    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+ " " + ingresarApellido);
+    console.log(" Turno  N° "+i+" Nombre: "+ingresarNombre + " / Apellido: "+ingresarApellido )
+}
+
+
+*/
+
+
+/*let resultado = 0;
+let primerNumero;
+let segundoNumero;
+
+
+function saludar() {
+    console.log("¡Hola estudiantes!");
+}
+function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero;
+        case "-":
+            return primerNumero - segundoNumero;
+        case "*":
+            return primerNumero * segundoNumero;
+        case "/":
+            return primerNumero / segundoNumero;
+        default:
+            return 0;
+    }
+}
+function mostrar(mensaje) {
+    console.log(mensaje);
+}
+function socilitarInformacion(){
+    let primerNumero = parseInt(prompt("Numero 1"));
+    let segundoNumero = parseInt(prompt("Segundo 2"));
+    let operador = prompt("Tipo de operador");
+    let respuesta = calculadora(primerNumero, segundoNumero, operador);
+    mostrar(respuesta);
+}
+
+
+
+saludar();
+socilitarInformacion(); 
+
+
+
+
+const suma  = (a,b) => { return a + b};
+const resta = (a,b) => a - b;
+const iva   = x => x * 0.21;
+const calcularPrecio = () => {
+    let precioProducto = parseInt(prompt("Precio Producto"));
+    let precioDescuento = parseInt(prompt("Precio Descuento"));
+    let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento); 
+    console.log(nuevoPrecio);
+}
+calcularPrecio()
+
+
+Ejercicio: Agencia vuelos:
+    * Se dará un valor determinado de tiquete
+    * Los niños tienen un 10% de descuento
+    Pedir mediante un prompt número de adultos
+    Pedir mediante un prompt número de niños
+    En base a esos 2 datos, imprimir en consola o con un alert el valor total por los tiquetes
+
+const valorTiquete = 500;
+const valorDescuentoNinos = 0.9;
+const multiplicacion = (a,b) => a*b;
+const suma = (a,b) => a+b;
+
+const calcularPrecio = () => {
+    let cantindadAdultos = parseInt(prompt("Cantindad de Adultos"));
+    let cantidadNinios = parseInt(prompt("Cantidad de Niños"));
+    let valorAdulto = multiplicacion(valorTiquete, cantindadAdultos);
+    let valorNinio = multiplicacion(multiplicacion(valorTiquete,cantindadAdultos),valorDescuentoNinos);
+    let valorFinal = suma(valorNinio,valorAdulto);
+    console.log("Precio de Adultos " +valorAdulto + " / Precio Niños " + valorNinio);
+    console.log(valorFinal)
+}
+calcularPrecio();
+*/
--- js/codigo.js
+++ js/codigo.js
@@ -1,25 +1,20 @@
+const valorProducto = 6200;
+const valorDescuentoTalleS = 0.9;
+const valorDescuentoTalleL = 0.8;
+const iva   = x => x * 0.21;
+const multiplicacion = (a,b) => a*b;
+const suma = (a,b) => a+b;
 
-let entrada = prompt("Ingresar un nombre");
-while(entrada != "ESC"){
-   switch (entrada) {
-       case "Brian":
-            alert("Bienvenido Brian");
-            break;
-        case "Valentin":
-            alert("Hola Valentín, bienvenido a tu página");
-            break;
-       default:
-           alert("No eres bienvenido a esta página")
-           break;
-   }
-   entrada = prompt("Ingresar un nombre");
+const precio = () =>{
+    let cantidadBuzosTalleS = parseInt(prompt("Cantidad de buzos talles S (10% de descuento)"));
+    let cantidadBuzosTalleM = parseInt(prompt("Cantidad de buzos talles M (No tiene descuento)"));
+    let cantidadBuzosTalleL = parseInt(prompt("Cantidad de buzos talles L (20% de descuento)"));
+    let valorTalleS =  multiplicacion(multiplicacion(suma(valorProducto, iva(valorProducto)),cantidadBuzosTalleS),valorDescuentoTalleS);
+    let valorTalleM =  multiplicacion(suma(valorProducto, iva(valorProducto)),cantidadBuzosTalleM);
+    let valorTalleL =  multiplicacion(multiplicacion(suma(valorProducto, iva(valorProducto)),cantidadBuzosTalleL),valorDescuentoTalleL);
+    let valorTotal = suma(suma(valorTalleS,valorTalleM),valorTalleL);
+    console.log("Precio de buzos talle S "+valorTalleS+ " / Precio de buzos talle M "+valorTalleM+ " / Precio de buzos talle L "+ valorTalleL);
+    console.log("Precio Total "+valorTotal);
+    alert("El precio total de su compra es de "+valorTotal)
 }
-for (let i = 1; i <= 2; i++) {
-    let ingresarNombre = prompt("Ingresar nombre");
-    let ingresarApellido = prompt("Ingresar apellido")
-    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre+ " " + ingresarApellido);
-    console.log(" Turno  N° "+i+" Nombre: "+ingresarNombre + " / Apellido: "+ingresarApellido )
-}
-
-
-
+precio();
